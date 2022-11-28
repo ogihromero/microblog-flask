0b6a19cafa82365a0b3e9fd7327a2f544259c2d6
Adriano Romero <adriano.romerofco@gmail.com>
2022-11-28 11:18:07 -0300

    feat: implementação de login e registro

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,16 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
-app.config.from_object(Config)
+app.config.from_object(Config) #Carregando configurações do arquivo config.py
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
+
 
 # Import realizado no final para evitar import circular
-from app import routes
+from app import routes, models
